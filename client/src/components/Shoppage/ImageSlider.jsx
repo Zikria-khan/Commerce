266f29b505649d818911f331cfa02eec34beed69
Practice <coding4443@gmail.com>
2024-11-20 02:01:57 -0800

    Changes

--- client/src/components/Shoppage/ImageSlider.jsx
+++ client/src/components/Shoppage/ImageSlider.jsx
@@ -6,9 +6,9 @@ const ImageSlider = () => {
   const images = [
     "./image1.jpg", // Path relative to the public folder
     "./image2.jpg",
-    "./image3.jpg"
-    "./image4.jpg"
-    "./image5.jpg"
+    "./image3.jpg",
+    "./image4.jpg",
+    "./image5.jpg",
   ];
 
   // State to track the current image index
